2d9c1bece19f9dda6ed622c31f39ab4a143b5192
lupangsogood <lupangsogood@gmail.com>
2019-03-10 04:29:46 +0700

    10 / 3 /2562
    ลองลิปสติคให้ได้
    เรียงโค้ดดีๆ
    ลองหาเซิฟเพื่อลง python

---
+++ Lip_Makeup/lipstick.py
@@ -0,0 +1,168 @@
+from pylab import *
+from scipy.interpolate import interp1d
+from skimage import color
+import numpy as np
+import cv2
+import dlib
+import imutils
+
+
+class LipMakeup_class():
+
+    def __init__(self):
+        self.im = 0
+        self.imOrg = 0
+        self.Rg = 0
+        self.Gg = 0
+        self.Bg = 0
+        self.height = 0
+        self.width = 0
+        self.up_left_end = 3 
+        self.up_right_end = 5
+        self.val = 0
+
+    def inter(lx, ly, k1='quadratic'):
+        unew = np.arange(lx[0], lx[-1] + 1, 1)
+        f2 = interp1d(lx, ly, kind=k1)
+        return f2, unew
+
+    def ext(a, b, i):
+        a, b = np.round(a), np.round(b)
+        x.extend(arange(a, b, 1, dtype=np.int32).tolist())
+        y.extend((ones(int(b - a), dtype=np.int32) * i).tolist())
+
+
+    def prepare(self):
+        detector  = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+        self.imOrg = self.im.copy()
+
+        figure()
+        height, width = self.imOrg.shape[:2]
+        print(self.imOrg.shape[:2])
+
+        imRoi = self.im.copy()
+        gray = cv2.cvtColor(self.imOrg, cv2.COLOR_BGR2GRAY)
+        rects = detector(gray, 1)
+
+        for (i,rect) in enumerate(rects):
+            shape = predictor(gray,rect)
+
+            #ค่าความกว้างของหน้า และ ความสูงของหน้า
+            HALF_FACE_RATIO = (0.695)
+            halfFace = shape.part(16).x * HALF_FACE_RATIO
+            FACE_HEIGHT_RATIO = (1.0 / 5)
+            heightFace = shape.part(8).y * FACE_HEIGHT_RATIO
+
+            #ค่าจุดกลางของหน้าเอาไว้จุดแก้มอีกหนึ่งข้าง
+            mid = shape.part(33).x 
+
+
+            x49 = shape.part(48).x
+            y49 = shape.part(48).y
+
+            x51 = shape.part(50).x
+            y51 = shape.part(50).y
+
+            x52 = shape.part(51).x
+            y52 = shape.part(51).y
+
+            x53 = shape.part(52).x
+            y53 = shape.part(52).y
+
+            x55 = shape.part(54).x
+            y55 = shape.part(54).y
+
+            x57 = shape.part(56).x
+            y57 = shape.part(56).y
+
+            x59 = shape.part(58).x
+            y59 = shape.part(58).y
+
+            x61 = shape.part(60).x
+            y61 = shape.part(60).y
+
+            x62 = shape.part(61).x
+            y62 = shape.part(61).y
+
+            x63 = shape.part(62).x
+            y63 = shape.part(62).y
+
+            x64 = shape.part(63).x
+            y64 = shape.part(63).y
+
+            x65 = shape.part(64).x
+            y65 = shape.part(64).y
+
+            x66 = shape.part(65).x
+            y66 = shape.part(65).y
+
+            x68 = shape.part(67).x
+            y68 = shape.part(67).y
+
+    
+            points = np.array([[x49,y49],[x51,y51],[x52,y52],[x53,y53],[x55,y55],[x57,y57],[x59,y59]
+            ,[x61,y61],[x62,y62],[x63,y63],[x64,y64],[x65,y65],[x66,y66],[x68,y68]])
+
+        # gets the points on the boundary of lips from the file
+        #file = np.loadtxt('pointpixel.txt')
+        #points = np.floor(file)
+        point_out_x = np.array((points[:len(points) // 2][:, 0]))
+        point_out_y = np.array(points[:len(points) // 2][:, 1])
+        point_in_x = (points[len(points) // 2:][:, 0])
+        point_in_y = points[len(points) // 2:][:, 1]
+
+        #figure()
+        #im = imread('Input.jpg')
+        #imOrg = im.copy()
+        # Code for the curves bounding the lips
+        o_u_l = inter(point_out_x[:up_left_end], point_out_y[:up_left_end])
+        o_u_r = inter(point_out_x[up_left_end - 1:up_right_end], point_out_y[up_left_end - 1:up_right_end])
+        o_l = inter([point_out_x[0]] + point_out_x[up_right_end - 1:][::-1].tolist(),
+                [point_out_y[0]] + point_out_y[up_right_end - 1:][::-1].tolist(), 'cubic')
+        i_u_l = inter(point_in_x[:up_left_end], point_in_y[:up_left_end])
+        i_u_r = inter(point_in_x[up_left_end - 1:up_right_end], point_in_y[up_left_end - 1:up_right_end])
+        i_l = inter([point_in_x[0]] + point_in_x[up_right_end - 1:][::-1].tolist(),
+                [point_in_y[0]] + point_in_y[up_right_end - 1:][::-1].tolist(), 'cubic')
+
+        x = []  # will contain the x coordinates of points on lips
+        y = []  # will contain the y coordinates of points on lips
+
+
+        for i in range(int(o_u_l[1][0]), int(i_u_l[1][0] + 1)):
+            ext(o_u_l[0](i), o_l[0](i) + 1, i)
+
+        for i in range(int(i_u_l[1][0]), int(o_u_l[1][-1] + 1)):
+            ext(o_u_l[0](i), i_u_l[0](i) + 1, i)
+            ext(i_l[0](i), o_l[0](i) + 1, i)
+
+        for i in range(int(i_u_r[1][-1]), int(o_u_r[1][-1] + 1)):
+            ext(o_u_r[0](i), o_l[0](i) + 1, i)
+
+        for i in range(int(i_u_r[1][0]), int(i_u_r[1][-1] + 1)):
+            xt(o_u_r[0](i), i_u_r[0](i) + 1, i)
+            ext(i_l[0](i), o_l[0](i) + 1, i)
+
+        # Now x and y contains coordinates of all the points on lips
+
+        self.val = color.rgb2lab((self.imOrg[x, y] / 255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
+        L, A, B = mean(self.val[:, 0]), mean(self.val[:, 1]), mean(self.val[:, 2])
+        L1, A1, B1 = color.rgb2lab(np.array((r / 255., g / 255., b / 255.)).reshape(1, 1, 3)).reshape(3, )
+        ll, aa, bb = L1 - L, A1 - A, B1 - B
+        self.val[:, 0] += ll
+        self.val[:, 1] += aa
+        self.val[:, 2] += bb
+
+    def apply_lipstick_func(self,imageFile,r,g,b):
+
+
+        self.im = imread(imageFile)
+        self.prepare
+        print(self.imOrg.flags)
+        #imOrg.setflags(write=True)
+        imOrg[x, y] = color.lab2rgb(self.val.reshape(len(x), 1, 3)).reshape(len(x), 3) * 255
+        gca().set_aspect('equal', adjustable='box')
+        imshow(self.imOrg)
+        show()
+        #imsave('output.jpg', im)
--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 from LipsEyeliner_Makeup import ApplyMakeup
 from Brush_Makeup.blush import BrushMakeup_class
+from Lip_Makeup.lipstick import LipMakeup_class
 import cv2
 import os
 
-path_image = 'Project_senior\Input3.jpg'
+path_image = 'Project_senior\input3.jpg'
 print(path_image)
 AM = ApplyMakeup()
 BM = BrushMakeup_class()
+LM = LipMakeup_class()
 
 
-output_file = AM.apply_lipstick(path_image,195,0,0 ) 
-output_file2 =AM.apply_liner(output_file)
+output_file = LM.apply_lipstick_func(path_image,255,0,0)
+#output_file2 =AM.apply_liner(output_file)
 
-test = BM.apply_brush(output_file2,255,0,255)
+#test = BM.apply_brush(output_file2,255,0,255)
 
