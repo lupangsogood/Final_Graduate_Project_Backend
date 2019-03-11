25cae1a2d24eff44afebee126f38092f51d257be
lupangsogood <lupangsogood@gmail.com>
2019-03-12 05:33:03 +0700

    12/3/2562
    ได้คิ้ว เหลือปรับคิ้วให้โอเครกว่านี้
    * API

--- test_eyebrown.py
+++ test_eyebrown.py
@@ -10,15 +10,12 @@ detector  = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 im_bg = cv2.imread('../FILE_OUTPUT_color_207_40_57.jpg')
-im_fg = cv2.imread('eyebrowL.png')
-
-
-cv2.imshow('Original',img)
-cv2.waitKey(0)
-## For Specs
-
-
-imOrg = img.copy()
+#READ IMAGE LIKE TRANsPARENT Background
+#im_fg = cv2.imread('bggray.jpg',flags=cv2.IMREAD_UNCHANGED)
+im_fg = cv2.imread('bgwhite.jpg',flags=cv2.IMREAD_UNCHANGED)
+im_fg = cv2.resize(im_fg,(100,100))
+#COPY IM_BG TO IMORG
+imOrg = im_bg.copy()
 gray = cv2.cvtColor(imOrg,cv2.COLOR_BGR2GRAY)
 rects = detector(gray,1)
 for (i,rect) in enumerate(rects):
@@ -37,3 +34,34 @@ for (i,rect) in enumerate(rects):
     w = shape.part(21).x+10
     h = shape.part(19).y+30
     cv2.rectangle(imOrg,(x,y),(w,h),(255,86,30),3)
+    
+    cv2.imshow("ROI",imOrg)
+    cv2.waitKey(0)
+
+mask = 255 * np.ones(im_fg.shape, im_fg.dtype)
+
+# The location of the center of the IM_FG in the IM_BG
+width, height, channels = im_bg.shape
+center = (int(height/2), int(width/2))
+point = (int((x+w)/2),int((y+h)/2))
+#print(center)
+print(point)
+
+
+# Seamlessly clone src into dst and put the results in output
+mixed_clone = cv2.seamlessClone(im_fg, im_bg, mask,point, cv2.MIXED_CLONE)
+#TEST POINT BY CIRCLE
+mixed_clone = cv2.circle(mixed_clone,point,3,(255,0,0),-1)
+# Write results
+cv2.imshow("MIXED",mixed_clone)
+cv2.waitKey(0)
+
+
+
+
+
+
+"""
+
+
+"""
