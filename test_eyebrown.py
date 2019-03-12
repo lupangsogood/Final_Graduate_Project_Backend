a54659958df3b8d47e44e9e0a579a0cea5a1b0f2
ANUSIT <lupangsogood@gmail.com>
2019-03-12 08:22:33 +0700

    update formular

--- test_eyebrown.py
+++ test_eyebrown.py
@@ -9,11 +9,11 @@ import matplotlib.image as mpimg
 detector  = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-im_bg = cv2.imread('../FILE_OUTPUT_color_207_40_57.jpg')
+im_bg = cv2.imread('Input3.jpg')
 #READ IMAGE LIKE TRANsPARENT Background
 #im_fg = cv2.imread('bggray.jpg',flags=cv2.IMREAD_UNCHANGED)
 im_fg = cv2.imread('bgwhite.jpg',flags=cv2.IMREAD_UNCHANGED)
-im_fg = cv2.resize(im_fg,(100,100))
+#im_fg = cv2.resize(im_fg,(100,100))
 #COPY IM_BG TO IMORG
 imOrg = im_bg.copy()
 
@@ -30,21 +30,32 @@ for (i,rect) in enumerate(rects):
 
     #ค่าจุดกลางของหน้าเอาไว้จุดแก้มอีกหนึ่งข้าง
     mid = shape.part(33).x-20
-    x = shape.part(17).x
-    y = shape.part(19).y-10
-    w = shape.part(21).x+10
-    h = shape.part(19).y+30
-    cv2.rectangle(imOrg,(x,y),(w,h),(255,86,30),3)
-    
-    cv2.imshow("ROI",imOrg)
-    cv2.waitKey(0)
 
+    xR = shape.part(17).x
+    yR = shape.part(19).y-10
+    wR = shape.part(21).x
+    hR = shape.part(19).y+30
+    cv2.rectangle(imOrg,(xR,yR),(wR,hR),(255,86,30),3)
+
+    xL = shape.part(22).x
+    yL = shape.part(24).y-10
+    wL = shape.part(26).x
+    hL = shape.part(24).y+30
+    cv2.rectangle(imOrg,(xL,yL),(wL,hL),(255,86,30),3)
+
+    #cv2.imshow("ROI",imOrg)
+    #cv2.waitKey(0)
+
+#เทสตำแหน่ง width , height ของ Graphic ให้สอดคล้องกับขนาด Bound คิ้ว
+im_fg = cv2.resize(im_fg,(int(wR/3),int(hR/3)))
+print(im_fg.shape[:])
 mask = 255 * np.ones(im_fg.shape, im_fg.dtype)
 
 # The location of the center of the IM_FG in the IM_BG
 width, height, channels = im_bg.shape
+#ลองเปลี่ยนจากขนาดของหน้าเป็นขนาด ROI คิ้ว
 center = (int(height/2), int(width/2))
-point = (int((x+w)/2),int((y+h)/2))
+point = (int((xR+wR)/2),int((yR+hR)/2))
 #print(center)
 print(point)
 
