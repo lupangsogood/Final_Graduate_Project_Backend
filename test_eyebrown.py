5bb42f355dbe8a9600685943a1845248fdddeb36
lupangsogood <lupangsogood@gmail.com>
2019-03-13 21:02:55 +0700

    13/3/2562
    รอปรับขนาดคิ้วจากกราฟฟิคใหม่
    กลบสีพื้นที่คิ้วให้ละเอียดขึ้น

--- test_eyebrown.py
+++ test_eyebrown.py
@@ -45,29 +45,32 @@ for (i,rect) in enumerate(rects):
     #cv2.imshow("ROI",imOrg)
     #cv2.waitKey(0)
 
-ROI = imROI[yR:hR,xR+10:wR]
-mask_ROI = 255 * np.ones(ROI.shape, ROI.dtype)
+#ROI = imROI[yR:hR,xR+10:wR]
+#mask_ROI = 255 * np.ones(ROI.shape, ROI.dtype)
 
-ROI_FOREHEAD =imOrg[yR+5:hR+5,xR:wR]
-cv2.imshow("ROI_FOREHEAD",ROI_FOREHEAD)
-cv2.waitKey(0)
+#ROI_FOREHEAD =imOrg[yR+5:hR+5,xR:wR]
+#cv2.imshow("ROI_FOREHEAD",ROI_FOREHEAD)
+#cv2.waitKey(0)
 #--------------------------------------------------------------
 #color = (ROI_FOREHEAD[0,0])
 #print(color)
 #---------------------------------------------------------------
-ROI_FOREHEAD= cv2.GaussianBlur(ROI_FOREHEAD,(3,3),5)
+#ROI_FOREHEAD= cv2.GaussianBlur(ROI_FOREHEAD,(3,3),5)
 #ROI_FOREHEAD = cv2.bilateralFilter(ROI_FOREHEAD,55,55,55)
-imOrg[yR:hR,xR:wR] = ROI_FOREHEAD
-cv2.imshow("ROI_FOREHEAD",imOrg)
-cv2.waitKey(0)
+#imOrg[yR:hR,xR:wR] = ROI_FOREHEAD
+#cv2.imshow("ROI_FOREHEAD",imOrg)
+#cv2.waitKey(0)
 #-----------เซต IM_BG ให้เท่ากับ ภาพที่เบลอคิ้วแล้ว
 im_bg = imOrg.copy()
 #-------------------------------
 
 im_fg = cv2.resize(im_fg,(int((wR+xR)*0.175),int((hR+yR)*0.15)))
-
 print(im_fg.shape[:])
 mask = 255 * np.ones(im_fg.shape, im_fg.dtype)
+cv2.imshow("TEST_GRAY",im_fg)
+cv2.waitKey(0)
+cv2.imshow("TEST_GRAY",mask)
+cv2.waitKey(0)
 
 # The location of the center of the IM_FG in the IM_BG
 width, height, channels = im_bg.shape
---
+++ test_eyebrown_2.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+import dlib
+import PIL
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+detector  = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+im_bg = cv2.imread('Input7.jpg')
+#READ IMAGE LIKE TRANsPARENT Background
+im_fg = cv2.imread('bgwhite.jpg',flags=cv2.IMREAD_UNCHANGED)
+
+imROI = im_bg.copy()
+imOrg = im_bg.copy()
+
+imOrg = cv2.resize(imOrg,(750,1000))
+gray = cv2.cvtColor(imOrg,cv2.COLOR_BGR2GRAY)
+rects = detector(gray,1)
+for (i,rect) in enumerate(rects):
+    shape = predictor(gray,rect)
+
+    #ค่าความกว้างของหน้า และ ความสูงของหน้า
+    HALF_FACE_RATIO = (0.695)
+    halfFace = shape.part(16).x * HALF_FACE_RATIO
+    FACE_HEIGHT_RATIO = (1.0 / 5)
+    heightFace = shape.part(8).y * FACE_HEIGHT_RATIO
+
+    #ค่าจุดกลางของหน้าเอาไว้จุดแก้มอีกหนึ่งข้าง
+    mid = shape.part(33).x-20
+
+    xR = shape.part(17).x
+    yR = shape.part(19).y
+    wR = shape.part(21).x
+    hR = shape.part(17).y
+
+    xL = shape.part(22).x
+    yL = shape.part(24).y-10
+    wL = shape.part(26).x
+    hL = shape.part(24).y+30
+
+ROI_BGR  = imOrg[yR:hR+4,xR:wR-10]
+ROI_GRAY = cv2.cvtColor(ROI_BGR,cv2.COLOR_BGR2GRAY)
+cv2.imshow("TEST_GRAY",ROI_GRAY)
+cv2.waitKey(0)
+
+ROI_GET_COLOR = imOrg[yR-50:hR-30,xR+20:wR]
+#cv2.imshow("ROI_GET_COLOR",ROI_GET_COLOR)
+#cv2.waitKey(0)
+### หาค่าสีของเส้นบนคิ้วแต่ละจุด
+eyeBrown_color = []
+eyeBrown_color =[(gray[shape.part(17).x,shape.part(17).y]),(gray[shape.part(18).x,shape.part(18).y]),(gray[shape.part(19).x,shape.part(19).y]),(gray[shape.part(20).x,shape.part(20).y]),(gray[shape.part(21).x,shape.part(21).y])]
+BGR_AVG = sum (eyeBrown_color)/len(eyeBrown_color)
+print (BGR_AVG)
+
+###ส่วนจัดการลูป REPLACE COLOR PIXEL
+get_color = (ROI_GET_COLOR[25,25])
+#print(get_color)
+height,width = ROI_BGR.shape[:2]
+print ("WIDTH,HEIGHT = ",width,height)
+
+
+
+for w in range(width):
+    for h in range(height):
+        print (w,h)
+        # ความสูงมาก่อน
+        color_gray = ROI_GRAY[h,w]
+        #print (color_gray)
+
+        if color_gray < (BGR_AVG/2):
+            print("CHECKED")
+            ROI_BGR[h,w] = get_color
+
+#cv2.imshow("ROI_GRAY",imOrg)
+#cv2.waitKey(0)
+im_bg = imOrg.copy()
+#---------------------------------------------------------------------------
+im_fg = cv2.resize(im_fg,(int((wR+xR)*0.175),int((hR+yR)*0.15)))
+im_fg= cv2.GaussianBlur(im_fg,(3,3),5)
+#im_fg = cv2.resize(im_fg,(100,100))
+mask = 255 * np.ones(im_fg.shape, im_fg.dtype)
+
+bg_width, bg_height, bg_channels = im_bg.shape
+#center = (int(bg_height/2), int(bg_width/2))
+point = (int((xR+wR)/1.95),int((yR+hR)/2))
+#print(center)
+#print(point)
+mixed_clone = cv2.seamlessClone(im_fg, im_bg, mask,point, cv2.MIXED_CLONE)
+#mixed_clone_2 = cv2.seamlessClone(im_fg, imOrg, mask,point, cv2.MIXED_CLONE)
+
+cv2.imshow("ROI_GRAY",mixed_clone)
+cv2.waitKey(0)
